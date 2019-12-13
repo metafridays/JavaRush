532d835f6bb9f9fd874e1855383064b3408b32c9
metafridays <metafridays@live.com>
2019-12-14 00:20:43 +0300

    4. JavaCollections task3202 passed validator

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3202/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3202;
+
+import java.io.*;
+
+/* 
+Читаем из потока
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        System.out.println(writer.toString());
+    }
+
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter writer = new StringWriter();
+        if (is == null) return writer;
+        while (is.available() != 0) {
+            writer.write(is.read());
+        }
+        return writer;
+    }
+}
