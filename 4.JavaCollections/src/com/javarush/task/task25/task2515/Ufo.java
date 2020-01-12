f8bd224408d2da51fee81fcc5064a8495003fe27
metafridays <metafridays@live.com>
2020-01-12 23:57:19 +0300

    4. JavaCollections task2515 stage 14 passed validator

---
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task25.task2515;
+
+public class Ufo extends BaseObject {
+
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 1, 0, 1},
+            {0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1; //-1..1
+        double dy = Math.random() * 2 - 1; //-1..1
+        x += dx;
+        y += dy;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius, Space.game.getHeight() / (double) 2 );
+        if (Math.random() > 0.9) fire();
+    }
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + radius));
+    }
+}
